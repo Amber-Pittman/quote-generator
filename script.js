39b82ef4adc9c9cb20234cd9896279086ae7bca5
Amber-Pittman <amber@amberpittman.com>
2022-04-24 16:43:25 -0400

    updated files for local quotes - working

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-let apiQuotes = []; 
+//let apiQuotes = []; 
 
-// Get Quotes from API
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes'
-    try {
-        const response = await fetch (apiUrl);
-        apiQuotes = await response.json();
-        console.log(apiQuotes[42]);
-    } catch (err) { 
-        // Catch Error Here
-        alert(err)
-    }
+// Show Random New Quote
+function newQuote() {
+    // Pick random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    console.log(quote);
 }
 
+// Get Quotes from API
+//async function getQuotes() {
+//    const apiUrl = 'https://type.fit/api/quotes'
+//    try {
+//        const response = await fetch (apiUrl);
+//        apiQuotes = await response.json();
+//        newQuote();
+//    } catch (err) { 
+//        // Catch Error Here
+//        alert(err)
+//    }
+//}
+
 // On Load
-getQuotes();
+//getQuotes();
+newQuote();
